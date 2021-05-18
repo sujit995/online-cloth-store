cb090c0f4ceb22fcb847044b3af0abf157bad785
sujit995 <sujitmishra402@gmail.com>
2021-05-18 15:22:04 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import './default.scss';
+import Header from './components/Header';
+import Homepage from './pages/Homepage';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <div className="main">
+        <Homepage />
+      </div>
     </div>
   );
 }
